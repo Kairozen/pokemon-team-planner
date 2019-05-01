6bb998bb555bc43afd02b1470fd13f5c5356d411
Kairozen <geoffbaudelet@yahoo.fr>
2019-05-01 20:23:21 +0200

    Updates

---
+++ precache-manifest.e590c802bc10bc96a5f72a8597c6bca7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47340bccfa0a72ad3da11896becd51c8",
+    "url": "/pokemon-team-planner/index.html"
+  },
+  {
+    "revision": "b606295db9718c45d9bc",
+    "url": "/pokemon-team-planner/static/css/main.edec22a7.chunk.css"
+  },
+  {
+    "revision": "9779843a97c5545e7c58",
+    "url": "/pokemon-team-planner/static/js/2.e422cace.chunk.js"
+  },
+  {
+    "revision": "b606295db9718c45d9bc",
+    "url": "/pokemon-team-planner/static/js/main.9d045b1e.chunk.js"
+  },
+  {
+    "revision": "6f67c205cd519e9107dd",
+    "url": "/pokemon-team-planner/static/js/runtime~main.b2e05496.js"
+  }
+]);
