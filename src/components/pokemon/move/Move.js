c44a3d1bcac48cebd41d0229de53964f9848cd5e
Kairozen <geoffbaudelet@yahoo.fr>
2019-05-04 23:15:40 +0200

    adding move info when hovering selected move

--- src/components/pokemon/move/Move.js
+++ src/components/pokemon/move/Move.js
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import titleize from 'titleize';
 import './Move.css';
 import { API } from '../../params'
+import Moveinfo from './moveinfo/Moveinfo';
 
 export default class Move extends Component {
     constructor() {
         super();
+        this.handleMouseHover = this.handleMouseHover.bind(this);
         this.state = {
+            isHovering: false,
             selectedMove: null
         };
 
         this.handleChange = this.handleChange.bind(this);
     }
 
+    handleMouseHover() {
+        this.setState({ isHovering: !this.state.isHovering })
+    }
+
     // Returns the object to send to the parent for changing attack type
-    moveTypesToSendToParent(oldType,newType) {
+    moveTypesToSendToParent(oldType, newType) {
         let dataFromChild = {
             old: oldType,
             new: newType
@@ -36,8 +43,8 @@ export default class Move extends Component {
             oldType = this.state.selectedMove.type.name;
         }
         if (event.target.value === "") {
-            this.setState({ selectedMove: null });
-            this.props.moveTypesCallback(this.moveTypesToSendToParent(oldType,newType));
+            this.setState({ isHovering: false, selectedMove: null });
+            this.props.moveTypesCallback(this.moveTypesToSendToParent(oldType, newType));
         }
         else {
             // Fetch data of the move
@@ -45,11 +52,11 @@ export default class Move extends Component {
             fetch(API + API_ROUTE)
                 .then(res => res.json())
                 .then((data) => {
-                    this.setState({ selectedMove: data });
-                    if(data.damage_class.name !== "status") {
+                    this.setState({ isHovering: false, selectedMove: data });
+                    if (data.damage_class.name !== "status") {
                         newType = data.type.name;
                     }
-                    this.props.moveTypesCallback(this.moveTypesToSendToParent(oldType,newType));
+                    this.props.moveTypesCallback(this.moveTypesToSendToParent(oldType, newType));
                 })
                 .catch(console.log);
         }
@@ -58,14 +65,22 @@ export default class Move extends Component {
     render() {
         const moves = this.props.moves;
         const selectedMove = this.state.selectedMove;
-        const typeClass = (selectedMove)?("select-"+selectedMove.type.name):("select-no-type");
+        const typeClass = (selectedMove) ? ("select-" + selectedMove.type.name) : ("select-no-type");
         return (
-            <select onChange={this.handleChange} className={"form-control move " + typeClass}>
-                <option key="" value=""></option>
-                {moves.map((move) =>
-                    <option key={move.move.name} value={move.move.name}>{titleize(move.move.name)}</option>
-                )}
-            </select>
+            <React.Fragment>
+                <select onMouseEnter={this.handleMouseHover}
+                    onMouseLeave={this.handleMouseHover}
+                    onChange={this.handleChange} className={"form-control move " + typeClass}>
+                    <option className="white" key="" value=""></option>
+                    {moves.map((move) =>
+                        <option key={move.move.name} value={move.move.name}>{titleize(move.move.name)}</option>
+                    )}
+                </select>
+                {
+                    selectedMove &&
+                    <Moveinfo isVisible={this.state.isHovering} move={selectedMove} />
+                }
+            </React.Fragment>
         );
     }
 }
---
+++ src/components/pokemon/move/moveinfo/Moveinfo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Moveinfo.css';
+import Type from '../../../types/type/Type';
+import titleize from 'titleize';
+export default function Moveinfo(props) {
+    const moveinfoClass = (props.isVisible) ? ('moveinfo-visible') : ('moveinfo-hidden');
+    const damageClassImage = './assets/' + props.move.damage_class.name + '.png';
+    let effect = props.move.effect_entries[0].effect.replace("$effect_chance", props.move.effect_chance);
+
+    return (
+        <div className={"moveinfo-container " + moveinfoClass}>
+            <h6>{titleize(props.move.name)} <div style={{ float: 'right' }}><Type type={props.move.type.name} /></div></h6>
+            <div className="row">
+                <div className="moveinfo-data col-5">
+                    <table>
+                        <tr>
+                            <th><img alt={props.move.damage_class.name} src={damageClassImage} /></th>
+                            <th></th>
+                        </tr>
+                        <tr>
+                            <th>Power</th>
+                            <th className='center'>{(props.move.power) ? (props.move.power) : ('--')}</th>
+                        </tr>
+                        <tr>
+                            <th>Accuracy</th>
+                            <th className='center'>{(props.move.accuracy) ? (props.move.accuracy) : ('--')}</th>
+                        </tr>
+                    </table>
+            </div>
+            <div className="moveinfo-data col-7">
+                <p>
+                    {effect}
+                </p>
+            </div>
+        </div>
+        </div >
+    );
+}
